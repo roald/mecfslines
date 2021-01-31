75f2173bb5cf1ceb22bcf407ef4f160e1685535e
Roald Dijkstra <roald.dijkstra@nedap.com>
2021-01-31 15:07:01 +0100

    Fix menu highlighting for products

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -60,7 +60,7 @@
             {{ __('Events') }}
           </x-responsive-nav-link>
 
-          <x-responsive-nav-link :href="route('products.index')" :active="request()->is('admin/events*')">
+          <x-responsive-nav-link :href="route('products.index')" :active="request()->is('admin/products*')">
             <x-slot name="path">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
             </x-slot>
@@ -158,7 +158,7 @@
             {{ __('Events') }}
           </x-nav-link>
 
-          <x-nav-link :href="route('products.index')" :active="request()->is('admin/events*')">
+          <x-nav-link :href="route('products.index')" :active="request()->is('admin/products*')">
             <x-slot name="path">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
             </x-slot>
