2ede703e467d8af83d652404c235469bb6a445f0
Roald Dijkstra <roald.dijkstra@nedap.com>
2021-02-02 14:18:27 +0100

    Style tag buttons

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -16,7 +16,7 @@
             @csrf
             @method('DELETE')
             <input type="hidden" name="tag_id" value="{{ $tag->id }}">
-            <button class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               <span class="sr-only">{{ __('Remove') }}</span>
               <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
@@ -45,7 +45,7 @@
               <option value="{{ $tag->id }}">{{ $tag->name }}</option>
             @endforeach
           </select>
-          <button type="submit" class="-ml-mx px-2 py-1 border border-gray-300 text-sm font-medium rounded-r-md text-gray-500 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+          <button type="submit" class="-ml-mx px-2 py-1 border-transparent text-sm font-medium rounded-r-md text-gray-400 bg-green-100 hover:bg-green-200 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
             <span class="sr-only">{{ __('Add') }}</span>
             <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
