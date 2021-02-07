0a98c240e441e25525de083c6dddb4865761f648
Roald Dijkstra <roald.dijkstra@nedap.com>
2021-02-07 17:07:56 +0100

    Add Preview button

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -40,6 +40,9 @@
             </h2>
           </div>
           <div class="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
+            <a href="{{ route('web.page', $page) }}" target="preview" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+              {{ __('Preview') }}
+            </a>
             <a href="{{ route('pages.remove', $page) }}" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
               {{ __('Delete') }}
             </a>
