c9981c0ea137d2f3467e89fb16486c5b2f0e0876
Roald Dijkstra <roald.dijkstra@nedap.com>
2021-03-18 12:14:26 +0100

    Hiding user invite for now

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,9 +9,9 @@
             {{ __('Users') }}
           </h2>
         </div>
-        <div class="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
+        {{-- <div class="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
           <a href="{{ route('users.create') }}" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{{ __('Invite user') }}</a>
-        </div>
+        </div> --}}
       </div>
 
     </div>
