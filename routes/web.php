f8db930de16d6efa2a9254030d01f6a7d4e6eb0b
Roald Dijkstra <roald.dijkstra@nedap.com>
2021-06-10 17:30:38 +0200

    Update subscription routes

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,7 @@ Route::prefix('admin')->middleware(['auth.admin', 'verified'])->group(function (
     Route::get('orders/{order}/calculate', [OrderController::class, 'calculate'])->name('orders.calculate');
     Route::get('orders/{order}/remove', [OrderController::class, 'remove'])->name('orders.remove');
     Route::resource('orders', OrderController::class)->except(['create']);
-    Route::resource('users.subscriptions', SubscriptionController::class)->shallow()->only(['show', 'edit', 'update']);
+    Route::resource('subscriptions', SubscriptionController::class)->only(['show', 'edit', 'update']);
     Route::get('payments/{payment}/refresh', [PaymentController::class, 'refresh'])->name('payments.refresh');
     Route::resource('orders.payments', PaymentController::class)->shallow()->only(['index', 'show']);
 });
