0a3350c514fee8d4664c758d9f7961fc48eb0327
Roald Dijkstra <roald.dijkstra@nedap.com>
2023-06-07 14:09:21 +0200

    Add database seed for first admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $admin = User::create([
+            'name' => __('Administrator'),
+            'email' => 'admin@talc.nl',
+            'role' => 'admin',
+            'password' => Hash::make('getstarted'),
+        ]);
+        $admin->markEmailAsVerified();
     }
 }
